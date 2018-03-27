b4a20ccf41b9c45347d69e25b06ca9372eb0e41c
Jack Dewinter <jackdewinter1@gmail.com>
2018-03-27 12:11:30 +0100

    top_up cycle implemented.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,13 @@ describe OysterCard do
   it "has a balance of 0 by default" do
     expect(card.balance).to eq 0
   end
+
+  describe "#top_up" do
+    it "adds money to balance" do
+      expect(card.top_up(10)).to eq card.balance
+    end
+
+  end
+
+
 end
